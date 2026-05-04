f0d46717f8c3087accd848632c8b18abba126141
Simvile <115091699+Simvile@users.noreply.github.com>
2026-05-04 21:54:29 +0200

    feat: add signin layout and authentication components (#1)
    Co-authored-by: Simvile Zimeme <zsimvile@gmail.com>

--- projects/app/src/app/app.config.ts
+++ projects/app/src/app/app.config.ts
@@ -1,8 +1,20 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { environment } from '../environments/environments.development';
 import { routes } from './app.routes';
+import { BASE_PATH, } from 'lib';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(), 
+    provideRouter(routes),
+    provideHttpClient(),
+    {
+      provide: BASE_PATH,
+      useValue: {
+        apiUrl: environment.apiUrl
+      }
+    }
+  ],
 };
